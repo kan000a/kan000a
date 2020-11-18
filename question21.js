564b93512099e4b00a6533b67a139c77e49ff686
kan000a <kan-suzuki@seraku.co.jp>
2020-11-18 12:16:58 +0900

    21_4

---
+++ question21.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+<!--
+function brack(){
+	document.body.style.backgroundColor="#000000"
+}
+function white(){
+	document.body.style.backgroundColor="#ffffff"
+}
+function blue(){
+	document.body.style.backgroundColor="#6633ff"
+}
+function red(){
+	document.body.style.backgroundColor="#ff6666"
+}
+function yellow(){
+	document.body.style.backgroundColor="#ffff66"
+}
+//-->
+</script>
